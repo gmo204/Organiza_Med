153345eb78ba587f4c3e962f22009882d05a74e1
Gilberto Mota <gilbertojr204@gmail.com>
2024-11-29 15:22:33 -0300

    editar atividade

---
+++ client/src/app/views/atividade/editar/editar-atividade.component.ts
@@ -0,0 +1,96 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { Router, RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
+import { NotificacaoService } from '../../../core/notificacao/notificacao.service';
+import { ListarMedicoViewModel } from '../../medico/models/medico.model';
+import { MedicoService } from '../../medico/services/medico.service';
+import { AtividadeService } from '../services/atividade.service';
+import { InserirAtividadeViewModel } from '../models/atividade.models';
+
+@Component({
+  selector: 'app-editar-atividade',
+  standalone: true,
+  imports: [
+    NgIf,
+    RouterLink,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatCardModule,
+    MatDatepickerModule
+  ],  templateUrl: './editar-atividade.component.html',
+})
+export class EditarAtividadeComponent {
+  atividadeForm: FormGroup
+  id?: string;
+
+  medicos$?: Observable<ListarMedicoViewModel[]>;
+
+  constructor(
+    private router: Router,
+    private medicoService: MedicoService,
+    private atividadeService:AtividadeService,
+    private notificacao: NotificacaoService
+  ) {
+    this.atividadeForm = new FormGroup({
+      tipo: new FormControl<string>('', [Validators.required]),
+      horaInicio: new FormControl<Date | null>(null, [Validators.required]),
+      horaFim: new FormControl<Date | null>(null, [Validators.required]),
+      medicoId: new FormControl<string | undefined>(undefined, [
+        Validators.required,
+      ]),
+    });
+  }
+
+  get tipo(){
+    return this.atividadeForm.get('tipo')
+  }
+  get horaInicio(){
+    return this.atividadeForm.get('horaInicio')
+  }
+  get horaFim(){
+    return this.atividadeForm.get('horaFim')
+  }
+
+  ngOnInit(): void {
+    this.medicos$ = this.medicoService.selecionarTodos();
+  }
+
+  editar(): void {
+    if (!this.id) {
+      this.notificacao.erro('Não foi possível recuperar o id requisitado!');
+      return;
+    }
+
+    const atividadeEditada: InserirAtividadeViewModel = this.atividadeForm.value;
+
+    this.atividadeService.editar(this.id, atividadeEditada).subscribe((res) => {
+      this.notificacao.sucesso(
+         `O registro ID [${res.tipo}] foi editado com sucesso!`
+      );
+
+      this.router.navigate(['/atividades']);
+    })
+  }
+
+  mapearNomeMedico(
+    id: string,
+    medicos: ListarMedicoViewModel[]
+  ): string {
+    const medico = medicos.find((medico) => medico.id === id);
+
+    return medico ? medico.nome : 'Medico não encontrado';
+  }
+}
