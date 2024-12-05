f7c3b7c51c7587aea6fd3f8cbaee3cd3094952c6
Gilberto Mota <gilbertojr204@gmail.com>
2024-12-05 14:49:38 -0300

    Testes

---
+++ server/OrganizaMed.Testes.Integracao/Compartilhado/RepositorioEmOrmTestsBase.cs
@@ -0,0 +1,18 @@
+﻿using OrganizaMed.Dominio.ModuloAutenticacao;
+using OrganizaMed.Infra.Orm.Compartilhado;
+using OrganizaMed.InfraOrm.ModuloAtividade;
+using OrganizaMed.InfraOrm.ModuloMedico;
+
+namespace OrganizaMed.Testes.Integracao.Compartilhado
+{
+    public abstract class RepositorioEmOrmTestsBase
+    {
+        protected OrganizaMedDbContext dbContext;
+
+        protected RepositorioMedicoOrm repositorioMedico;
+        protected RepositorioAtividadeOrm repositorioAtividade;
+
+        protected Usuario usuarioAutenticado;
+
+    }
+}
